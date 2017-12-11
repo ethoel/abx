83c7325598beeb7ea5c9c892da62c6ac12f13642
Elliott Hoel <elliott.hoel@gmail.com>
2017-12-10 17:08:18 -0800

    wooork

--- imports/ui/gramToColor.js
+++ imports/ui/gramToColor.js
@@ -1,8 +1,8 @@
 export function gramToColor(gram) {
   console.log("in gramToColor fxn: " + gram);
-  if (this.gram > 0) {
+  if (gram > 0) {
     return "#663399";
-  } else if (this.gram < 0) {
+  } else if (gram < 0) {
     return "#DDA0DD";
   } else {
     return "#696969";
