5d68521b8db2a10d48c96cbfcb8b4e6f139b61de
Elliott Hoel <elliott.hoel@gmail.com>
2017-12-14 10:37:24 -0800

    got rid of console.logs

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -8,7 +8,6 @@ import { gramToColor, bugCompare, abxCompare, infxnCompare } from './functions.j
 import './body.html';
 
 Meteor.startup(function () {
-  console.log("startup");
   Session.set("widestLabel", 0);
 });
 
@@ -20,30 +19,25 @@ Template.body.helpers({
     return Abx.find({}).fetch().sort(abxCompare);
   },
   coveredBugs: function () {
-    console.log(Session.get("lastAbxClick"));
+    Session.get("lastAbxClick");
     var checkedAbxIds = $(".abx:checked").map(function () {
       var _id = parseInt(this.value);
       return {abx: _id};
     }).get();
 
-    //console.dir(checkedAbxIds);
-    
     if (checkedAbxIds.length > 0) {
       return Bugs.find({$or: checkedAbxIds}).fetch().sort(bugCompare);
-      //return Bugs.find({$or: [{abx: 22}, {abx: 24}]});
     } else {
       return "";
     }
   },
   bugs: function () {
-    console.log(Session.get("lastInfxnClick"));
-    console.log("in bugs");
+    Session.get("lastInfxnClick");
 
     var checkedInfxnIds = $(".infxns:checked").map(function () {
       var _id = parseInt(this.value);
       return _id;
     }).get();
-    console.dir(checkedInfxnIds);
 
     var bugIds = [];
     for (var i = 0; i < checkedInfxnIds.length; i++) {
@@ -53,32 +47,24 @@ Template.body.helpers({
 
 
     if (bugIds.length > 0) {
-      console.log("11111111111111  " + $(".bugs:checked").length);
       $(".bugs:checked").each(function () {
-        console.log("does this happen? " + this.value);
-        console.dir(bugIds);
         if ($.inArray(parseInt(this.value), bugIds) < 0) {
-          console.log("does this happen? yes for " + this.value);
           $(this).prop('checked', false);
         }
       });
       Session.set("lastBugsClick", (new Date()).getTime());
-      console.log("222222222222222  " + $(".bugs:checked").length);
       return Bugs.find({_id: {$in: bugIds}}).fetch().sort(bugCompare);
     } else {
       return Bugs.find({}).fetch().sort(bugCompare);
     }
   },
   abxForBugs: function () {
-    console.log(Session.get("lastBugsClick"));
-    console.log("333333333333");
+    Session.get("lastBugsClick");
     var checkedBugsIds = $(".bugs:checked").map(function () {
       var _id = parseInt(this.value);
       return {bugs: _id};
     }).get();
 
-    console.dir(checkedBugsIds);
-    
     if (checkedBugsIds.length > 0) {
       return Abx.find({$and: checkedBugsIds}).fetch().sort(abxCompare);
     } else {
@@ -101,16 +87,9 @@ Template.coveredBug.helpers({
 
 Template.body.events({
   "click .abx": function (e) {
-    console.log("in function click .abx");
     Session.set("lastAbxClick", (new Date()).getTime());
   },
   "click .bugs": function (e) {
-    console.log("in function click .bugs" + $(".bugs:checked").length);
-    console.dir(this);
-    console.log("in function click .bugs" + $(".bugs").length);
-    //if (!$(this).prop("checked")) {
-    //  $("#allBugs").prop("checked", false);
-    //} else
     if ($(".bugs").not(":checked").length > 0) {
       $("#allBugs").prop("checked", false);
     } else { 
@@ -119,28 +98,23 @@ Template.body.events({
     Session.set("lastBugsClick", (new Date()).getTime());
   },
   "click .infxns": function (e) {
-    console.log("in function click .infxns");
     Session.set("lastInfxnClick", (new Date()).getTime());
   },
   "click #allBugs": function (e) {
-    console.log("in function click .allBugs");
     $(".bugs").prop("checked", $("#allBugs").prop("checked"));
     Session.set("lastBugsClick", (new Date()).getTime());
   },
 });
 
 Template.bug.onRendered(function () {
-  console.log("bug.onRendered");
   var widestLabel = Session.get("widestLabel");
   var currentLabel = this.$('label').width();
 
-  console.log(currentLabel + " > " + widestLabel);
   if ( currentLabel > widestLabel ) {
     Session.set("widestLabel", currentLabel);
     $('#widestLabel').remove();
     $('<style id="widestLabel">.equalWidth { width: ' + currentLabel + 'px; }</style>').appendTo('head');
   }
-  console.log("widestLabel: " + Session.get("widestLabel"));
   this.$('label').addClass("equalWidth");
 
   if ($(".bugs").not(":checked").length > 0) {
@@ -159,31 +133,25 @@ Template.bug.onDestroyed(function () {
 });
 
 Template.antibiotic.onRendered(function () {
-  console.log("bug.onRendered");
   var widestLabel = Session.get("widestLabel");
   var currentLabel = this.$('label').width();
 
-  console.log(currentLabel + " > " + widestLabel);
   if ( currentLabel > widestLabel ) {
     Session.set("widestLabel", currentLabel);
     $('#widestLabel').remove();
     $('<style id="widestLabel">.equalWidth { width: ' + currentLabel + 'px; }</style>').appendTo('head');
   }
-  console.log("widestLabel: " + Session.get("widestLabel"));
   this.$('label').addClass("equalWidth");
 });
 
 Template.infxn.onRendered(function () {
-  console.log("infxn.onRendered");
   var widestLabel = Session.get("widestLabel");
   var currentLabel = this.$('label').width();
 
-  console.log(currentLabel + " > " + widestLabel);
   if ( currentLabel > widestLabel ) {
     Session.set("widestLabel", currentLabel);
     $('#widestLabel').remove();
     $('<style id="widestLabel">.equalWidth { width: ' + currentLabel + 'px; }</style>').appendTo('head');
   }
-  console.log("widestLabel: " + Session.get("widestLabel"));
   this.$('label').addClass("equalWidth");
 });
